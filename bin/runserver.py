ed2612ff764735f366b06ec69fae84c153e73027
Fidel Ramirez <fidelramirez@Fidels-MacBook-Air.local>
2015-11-27 00:36:57 +0100

    parameters are now read from a config file. Home now different template as browser. Added search for genes.

--- bin/runserver.py
+++ bin/runserver.py
@@ -1,8 +1,8 @@
 
 from hicbrowser import app
 import sys
-if len(sys.argv) > 2:
-    port = sys.argv[3]
+if len(sys.argv) > 1:
+    port = sys.argv[2]
 else:
     port = None
 app.run(host='0.0.0.0', debug=False, port=port)
--- hicbrowser/views.py
+++ hicbrowser/views.py
@@ -6,16 +6,63 @@ from os.path import basename, exists
 
 import hicexplorer.trackPlot
 
-#track_file = "/data/manke/group/ramirez/tools/HiCBrowser/tracks.ini"
-track_file = sys.argv[1]
-print track_file
+from ConfigParser import SafeConfigParser
+config = SafeConfigParser()
+config.readfp(open(sys.argv[1], 'r'))
 
+track_file = config.get("browser", "tracks")
+print track_file
 trp = hicexplorer.trackPlot.PlotTracks(track_file, fig_width=60, dpi=70)
 
-#img_root = "/data/manke/group/ramirez/tools/HiCBrowser/hicbrowser/static/tmp"
+img_root = config.get('browser', 'images folder')
 
-img_root = sys.argv[2]
+# initialize TAD interval tree
+tads_file = config.get('general', 'TAD intervals')
+global tads_intval_tree
+tads_intval_tree = hicexplorer.trackPlot.file_to_intervaltree(tads_file)
 
+# initialize gene name to position mapping
+genes = config.get('general', 'genes')
+
+global gene2pos
+gene2pos = {}
+
+# initialize tads tracks
+track_file = config.get('general', 'tracks')
+tads = hicexplorer.trackPlot.PlotTracks(track_file, fig_width=60, dpi=70)
+
+tad_img_root = config.get('general', 'images folder')
+
+
+with open(genes, 'r') as fh:
+    for line in fh.readlines():
+        if line.startswith('browser') or line.startswith('track') or line.startswith('#'):
+            continue
+        chrom, start, end, name = line.strip().split("\t")[0:4]
+        try:
+            start = int(start)
+            end = int(end)
+        except ValueError:
+            sys.stderr.write("Problem with line {}".format(line))
+            pass
+        gene2pos[name.lower()] = (chrom, start, end)
+
+def get_TAD_for_gene(gene_name):
+    """
+    Returs the TAD position of a given gene name
+
+    :param gene_name:
+    :return:
+    """
+    if gene_name in gene2pos:
+        gene_name = gene_name.strip().lower()
+        # get gene position
+        chrom, start, end = gene2pos[gene_name]
+        tad_pos = tads_intval_tree[chrom].find(start, end)[0]
+
+        return chrom, tad_pos.start, tad_pos.end
+    else:
+        return None
 
 def get_region(region_string):
     """
@@ -47,8 +94,29 @@ def get_region(region_string):
 
 @app.route('/', methods=['GET'])
 def index():
+    gene_name = request.args.get('search', None)
+    if gene_name:
+        # see if the gene is known
+        tad_pos = get_TAD_for_gene(gene_name)
+        if tad_pos:
+            chromosome, start, end = tad_pos
+            start -= 50000
+            end += 50000
+            # plot
+            outfile = "{}/{}_{}_{}.png".format(tad_img_root,
+                                               chromosome,
+                                               start,
+                                               end)
+            if not exists(outfile):
+                tads.plot(outfile, chromosome, start, end)
+
+            return send_file(outfile, mimetype='image/png')
+
+    return render_template("index.html")
+
+@app.route('/browser', methods=['GET'])
+def browser():
     try:
-        #import ipdb;ipdb.set_trace()
         print request.args['region'].strip()
         chromosome, start, end = get_region(request.args['region'].strip())
         if end - start < 100000:
