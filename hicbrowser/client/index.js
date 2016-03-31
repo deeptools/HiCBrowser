259fbd2d861015f1870a894ca7ab5a155d5b6474
jVillaveces <secevalliv@gmail.com>
2016-03-31 16:40:51 +0200

    organized imports

--- hicbrowser/client/index.js
+++ hicbrowser/client/index.js
@@ -1,36 +1,31 @@
 //Required libs
 global.jQuery = $ = require('jquery');
+Backbone = require('backbone');
+Backbone.$ = jQuery;
+_ = require('underscore');
 require('bootstrap');
 
-function search(){
-    
-    var gene_name = $('#search_input').val();
-    
-    if( gene_name.length === 0 ) return;
-    
-    $.get('/tad?gene=' + gene_name, function(res) {
-        
-        if(res.error){
-            /* TODO  */
-            return;
-        }
-        
-        res = JSON.parse(res);
-        
-        $('#content').css({opacity: 0.0, visibility: 'hidden'});
-        $('.site-heading').addClass('header_small');
-        
-        $('#content').html(getTADView(res));
-        $('#content').css({opacity: 0.0, visibility: 'visible'}).animate({opacity: 1.0}, 800);
-        
+// Views
+var Loading = require('./js/views/loading');
+var loading = new Loading({el:'body'});
+
+App = {};
+
+//Router
+App.router = require('./js/router');
+
+App.init = function(){
+
+    // listen to ajax
+    $(document).ajaxStart(function() {
+        loading.show();
+
+    }).ajaxStop(function() {
+        setTimeout(loading.hide, 800);
     });
     
-}
+    // Render loading
+    loading.render();
+};
 
-function getTADView(data){
-    
-    return '<div class="col-lg-12"><h1 class="page-header">'+ data.gene +'<small> chromosome ' + data.chromosome +' (start: ' + data.start + ', end: ' + data.end + ')</small></h1><a href="/browser?region=' + data.chromosome + ':' + data.start + '-' + data.end + '"><img class="img-responsive" src="' + data.img + '" alt=""></a><hr></div>';
-    
-}
-
-$('#search').click(search);
+App.init();
