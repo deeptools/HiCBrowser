94baee7c1c4b78909e6138d3d4ee0911bb0523f4
vivekbhr <bhardwaj@ie-freiburg.mpg.de>
2016-05-20 15:35:51 +0200

    avoid reloading content in debug mode

--- bin/runserver.py
+++ bin/runserver.py
@@ -5,4 +5,4 @@ if len(sys.argv) > 1:
     port = int(sys.argv[2])
 else:
     port = None
-views.app.run(host='0.0.0.0', debug=True, port=port, processes=20)
+views.app.run(host='0.0.0.0', debug=True,use_reloader=False, port=port, processes=20)
